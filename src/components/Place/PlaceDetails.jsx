8b83424ef154055ef856c7fbcc62284058fdf9b0
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-10-04 10:01:29 +0300

    Edit place details component to add review feature

--- src/components/Place/PlaceDetails.jsx
+++ src/components/Place/PlaceDetails.jsx
@@ -27,6 +27,10 @@ function PlaceDetails({ place }) {
       <CardContent>
         <Typography gutterBottom variant="h5">{place.name}</Typography>
         <Box display="flex" justifyContent="space-between">
+          <Rating value={Number(place.rating)} readOnly />
+          <Typography gutterBottom variant="subtitle1">out of {place.num_reviews} reviews</Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between">
           <Typography variant="subtitle1">Price Level</Typography>
           <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
         </Box>
